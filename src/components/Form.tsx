1b971cb997800ca2fa4179ed302974d1fa232130
Isaac <mayne.isaac@gmail.com>
2024-12-20 18:59:05 +0100

    add reducer

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from "react"
+import { useState, ChangeEvent, FormEvent } from "react"
 import categories from "../data/categories"
 import { Activity } from "../types"
 export default function Form() {
@@ -8,17 +8,28 @@ export default function Form() {
         calories: 0
     })
 
-    const handleChange = (e : ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
-        const isNumberField = ['category', 'calories'].includes(e.target.id)        
+    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ['category', 'calories'].includes(e.target.id)
 
         setActivity({
             ...activity,
             [e.target.id]: isNumberField ? +e.target.value : e.target.value
         })
-    } 
+    }
+
+    const isValidActivity = () => {
+        const { name, calories } = activity
+        return name.trim() !== '' && calories > 0
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log('submit', activity);
+
+    }
 
     return (
-        <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <form className="space-y-5 bg-white shadow p-10 rounded-lg" onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="category" className="font-bold">Categoria:</label>
                 <select id="category" className="border border-slate-300 p-2 rounded-lg w-full bg-white" value={activity.category} onChange={handleChange}>
@@ -36,7 +47,7 @@ export default function Form() {
                 <input type="number" id="calories" value={activity.calories} onChange={handleChange} className="border border-slate-300 p-2 rounded-lg w-full" placeholder="Calorias ej. 300 o 500" />
             </div>
 
-            <input type="submit" value="Guardar comida o guardar ejericio" className="bg-gray-800 hover:bg-gray-900 text-white p-2 uppercase w-full" />
+            <input disabled={!isValidActivity()} type="submit" value={activity.category === 1 ? 'Agregar comida' : 'Agregar ejercicio'} className="bg-gray-800 hover:bg-gray-900 text-white p-2 uppercase w-full disabled:opacity-10" />
         </form>
     )
 }
---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,17 @@
+import { Activity } from "../types"
+
+export type ActivityActions = {
+    
+}
+type ActivityState = {
+    activities: Activity[]
+}
+export const initialState: ActivityState = {
+    activities: []
+}
+export const activityReducer = (
+    state: ActivityState = initialState,
+    action: ActivityActions
+) => { 
+    
+}
