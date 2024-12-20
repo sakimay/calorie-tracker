f247dd829c19b1452a9f270a0dbd1c105c6b8538
Isaac <mayne.isaac@gmail.com>
2024-12-20 18:33:25 +0100

    add state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,23 +7,30 @@ export default function Form() {
         calories: 0
     })
 
+    const handleChange = (e) => {        
+        setActivity({
+            ...activity,
+            [e.target.id]: e.target.value
+        })
+    } 
+
     return (
         <form className="space-y-5 bg-white shadow p-10 rounded-lg">
             <div className="grid grid-cols-1 gap-3">
-                <label htmlFor="name" className="font-bold">Categoria:</label>
-                <select id="name" className="border border-slate-300 p-2 rounded-lg w-full bg-white" value={activity.category} onChange={(e) => setActivity({ ...activity, category: Number(e.target.value) })}>
-                    {categories.map((category) => (
-                        <option key={category.id} value={category.id}>{category.name}</option>
+                <label htmlFor="category" className="font-bold">Categoria:</label>
+                <select id="category" className="border border-slate-300 p-2 rounded-lg w-full bg-white" value={activity.category} onChange={handleChange}>
+                    {categories.map((cat) => (
+                        <option key={cat.id} value={cat.id}>{cat.name}</option>
                     ))}
                 </select>
             </div>
             <div className="grid grid-cols-1 gap-3">
-                <label htmlFor="activity" className="font-bold">Actividad:</label>
-                <input type="text" id="activity" value={activity.name} onChange={(e) => setActivity({ ...activity, name: e.target.value })} className="border border-slate-300 p-2 rounded-lg w-full" placeholder="ej. Comida, Zumo de naranja, Ensalada, Ejercicio, Pesas, Bicicleta" />
+                <label htmlFor="name" className="font-bold">Actividad:</label>
+                <input type="text" id="name" value={activity.name} onChange={handleChange} className="border border-slate-300 p-2 rounded-lg w-full" placeholder="ej. Comida, Zumo de naranja, Ensalada, Ejercicio, Pesas, Bicicleta" />
             </div>
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="calories" className="font-bold">Calorias:</label>
-                <input type="number" id="calories" value={activity.calories} onChange={(e) => setActivity({ ...activity, calories: Number(e.target.value) })} className="border border-slate-300 p-2 rounded-lg w-full" placeholder="Calorias ej. 300 o 500" />
+                <input type="number" id="calories" value={activity.calories} onChange={handleChange} className="border border-slate-300 p-2 rounded-lg w-full" placeholder="Calorias ej. 300 o 500" />
             </div>
 
             <input type="submit" value="Guardar comida o guardar ejericio" className="bg-gray-800 hover:bg-gray-900 text-white p-2 uppercase w-full" />
