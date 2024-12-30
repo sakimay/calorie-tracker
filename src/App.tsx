f3f9db47798d43aa1aba60f827b14e72302a5e9d
Isaac <mayne.isaac@gmail.com>
2024-12-30 23:25:35 +0100

    add more stats

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
       <header className="bg-lime-600 py-3">
-        <div className="max-w-4xl mx-auto flex justify-between">
+        <div className="max-w-4xl mx-auto flex justify-between items-center">
           <h1 className="text-center text-xl font-bold text-white uppercase">
             Contador de calorias
           </h1>
--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -11,10 +11,11 @@ export default function CalorieTracker({ activities }: CalorieTrackerProps) {
     //contadores
     const caloriesCosumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
     const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities])
+    const netCalories = useMemo(() => caloriesCosumed - caloriesBurned, [activities])
     return (
         <>
             <h2 className="text-4xl font-black text-center text-white">Resumen de Calorias</h2>
-            <div className="flex flex-col items-center md:flex-row md:justify-evenly gap-5 mt-10">
+            <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
                 <CalorieDisplay
                     calories={caloriesCosumed}
                     text="Consumidas"
@@ -22,6 +23,10 @@ export default function CalorieTracker({ activities }: CalorieTrackerProps) {
                 <CalorieDisplay
                     calories={caloriesBurned}
                     text="Ejercicio"
+                />
+                <CalorieDisplay
+                    calories={netCalories}
+                    text="Diferencia"
                 />
             </div>
         </>
