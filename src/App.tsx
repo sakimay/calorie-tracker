3e9f7630e156c20e60f6acba723c435fd48366d0
Isaac <mayne.isaac@gmail.com>
2024-12-30 23:23:14 +0100

    resumen calories

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Form from './components/Form'
 import { useEffect, useMemo, useReducer } from 'react'
 import { activityReducer, initialState } from './reducers/activity-reducer'
 import ActivityList from './components/ActivityList'
+import CalorieTracker from './components/CalorieTracker'
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState)
@@ -33,6 +34,14 @@ function App() {
           <Form
             dispatch={dispatch}
             state={state}
+          />
+        </div>
+      </section>
+
+      <section className='bg-gray-800 py-10'>
+        <div className='max-w-4xl mx-auto'>
+          <CalorieTracker 
+            activities={state.activities}
           />
         </div>
       </section>
